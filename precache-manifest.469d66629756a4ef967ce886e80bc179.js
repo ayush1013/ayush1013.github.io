b313f3025ffb43d81899a580536f5e2e4501eb25
Ayush Verma <ayush98277@gmail.com>
2023-03-27 14:10:13 +0530

    Updates

---
+++ precache-manifest.469d66629756a4ef967ce886e80bc179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f45ffec15fa27202f2a",
+    "url": "/static/css/main.e14acb0c.chunk.css"
+  },
+  {
+    "revision": "0f45ffec15fa27202f2a",
+    "url": "/static/js/main.0f45ffec.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "6427d2d76925ee099bba",
+    "url": "/static/js/2.6427d2d7.chunk.js"
+  },
+  {
+    "revision": "9533ad4d11ae861fee58",
+    "url": "/static/js/runtime~main.9533ad4d.js"
+  },
+  {
+    "revision": "9a3dd317712d37b68ac2e8f82cdf97de",
+    "url": "/index.html"
+  }
+];
